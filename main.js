ad91c8a8b577db9e6cb72c5c362a2735d250c824
nasuunasuu <nasuu0612@gmail.com>
2025-07-05 20:58:24 +0900

    プレイヤータイプ判定の追加

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 // main.js
-import { createBoard, updateBoard } from './js/board.js';
-import { createEmptyBoard, dropDisc, checkWin } from './js/game.js';
+import { createEmptyBoard, createBoard, updateBoard } from './js/board.js';
+import { dropDisc, checkWin } from './js/game.js';
 import { getBestMove } from './js/ai.js';
 import { showStartScreen, showGameScreen } from './js/screen.js'
-import { getPlayerTypeAndName } from './js/start.js';
+import { getPlayerTypeAndName } from './js/playerInfo.js';
 
 const board = createEmptyBoard();
 const boardDiv = document.getElementById("board");
 const message = document.getElementById("message");
 const startButton = document.getElementById("start-button");
 let currentPlayer = 1;
-let player1, player2;
+let player1, player2, player1Type, player2Type;
 
 showStartScreen();
 
 startButton.addEventListener("click", () => {
   player1 = getPlayerTypeAndName("one-player-type", "one-player-name")||"おまえ";
   player2 = getPlayerTypeAndName("two-player-type", "two-player-name")||"てめえ";
+  player1Type = player1 === "AI" ? "AI" : "human";
+  player2Type = player2 === "AI" ? "AI" : "human";
+
   showGameScreen();
   message.textContent = player1 + "のばん";
   createBoard(boardDiv, handlePlayerMove);
